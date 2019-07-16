fd6e370507704165853dd108cacc3d0249e1d4da
gtfpga-server <gtfpga-server@caps.edu>
2019-07-16 16:39:32 -0400

    checked some stuff for running the case

--- ShowRunner.py
+++ ShowRunner.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 from yamlParseObjects.yamlObjects import *
 import logging 
 from eventManager.eventsLogger import * 
@@ -14,6 +16,7 @@ for p in simConfig.codeBase:
 
 from autoRTDS import Trial
 from controls import Control, InternalControl
+import case_Setup
 
 variables = getAllVariableConfigs('variables.yaml')
 for v in variables: 
@@ -33,15 +36,17 @@ class PGM_control(Control):
     NAME = 'PGM_control'
 
     def __init__(self, ctrl_str, controls_dir):
+        super().__init__(ctrl_str, controls_dir)
         logging.info('Instantiating the PGM control')
         self.start_file_name = None
         self.ctrl_str        = ctrl_str
         self.start_file      = None
         self.controls_dir    = controls_dir
 
-
         self.simulation = self.pull_case(f'{case_Setup.CEF_BASE_DIR}/MVDC_SPS/RTDS_V5.007/fileman/PGM_SampleSystem/V2/')
         self.dft_file = self.simulation.dft_file
+        self.simulation.set_run_function('start_case()')
+        #self.simulation.set_run_script('Start_Case.scr')
         
         self.simulation.set_int_control(internal_ctrl = True)
 
@@ -51,5 +56,6 @@ controlsToRun = [myControl]
 
 testDropLoc = Trial.init_test_drop(myControl.NAME)
 ctrl = myControl
+ctrl.initialize()
 trial = Trial(ctrl, ctrl.simulation, testDropLoc)
 trial.run()
