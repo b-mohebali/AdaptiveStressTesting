287d0c4b3240406287f59a3ae245898d0fc05e19
Behshad <bm12m@fsu.edu>
2021-09-21 22:15:30 -0400

    Progress evaluation

--- progressEvaluation.py
+++ progressEvaluation.py
@@ -1,4 +1,4 @@
-# # ! /usr/bin/python3
+#! /usr/bin/python3
 
 from ActiveLearning.visualization import setFigureFolder, SaveInformation, plotSpace
 from ActiveLearning.benchmarks import TrainedSvmClassifier
